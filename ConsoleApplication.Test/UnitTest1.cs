30f1a81263fd8ecec86ca68f894d4a50d2e43686
Raxid <radoslav.stenke@gmail.com>
2015-04-20 15:35:35 +0200

    Testing Exercise
    20.04.15

---
+++ ConsoleApplication.Test/UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ConsoleApplication1;
+namespace ConsoleApplication.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void ReturningZeroTest()
+        {           
+            var result = Calculator.Add(" , ");
+            Assert.AreEqual(result, 0);
+        }
+    }
+}
---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ConsoleApplication1
+{
+   public class Calculator
+    {
+       public static int Main(string[] args)
+       {
+           return 0;
+       }
+        public static int Add(string Stream)
+        {
+            string stream = Stream;
+            string num1,num2;
+            string [] split = stream.Split(new Char[] { ' ', ' ' });            
+            num1 = split[0];
+            num2 = split[2];
+            if (num1 == "" && num2 == "")
+            {
+                return 0;
+            }
+            return Int32.Parse(num1)+ Int32.Parse(num2);
+        }
+    }
+}
